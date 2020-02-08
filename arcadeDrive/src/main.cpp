4aab5b863746671daa9cbc36e79cdf781b4e714f
Sean <smfreeman989@gmail.com>
2020-02-08 15:12:24 -0600

    Finished all motor code 1st draft + dolley event

--- arcadeDrive/src/main.cpp
+++ arcadeDrive/src/main.cpp
@@ -35,14 +35,14 @@ vex::motor rightBeltMotor(vex::PORT7);
 
 vex::motor dolleyMotor(vex::PORT8);
 
-
+//Function for scaling down sensitivity of motors to stick at low throw
 int scale(int axisVal){
-
   double scaledVal;
   scaledVal = ((double)axisVal * abs(axisVal)) / 100.0;
   int newVal = (int)scaledVal;
   return newVal;
 }
+
 
 
 //vex::motor leftArmMotor(vex::####);
@@ -53,39 +53,88 @@ vex::controller fakeXbox = vex::controller();
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+
+
   while(1) {
      //grab controller axis values
      int stickAxis3 = fakeXbox.Axis3.value();
      int stickAxis4 = fakeXbox.Axis4.value();
      int stickAxis2 = fakeXbox.Axis2.value();
      int stickAxis1 = fakeXbox.Axis1.value();
-
      //scale axis values
      int scaledAxis3 = scale(stickAxis3);
      int scaledAxis4 = scale(stickAxis4);
      int scaledAxis2 = scale(stickAxis2);
      int scaledAxis1 = scale(stickAxis1);
+     //the rest are buttons whose values need not be read in  and stored
 
-     //calculate motor velocity settings
-     int leftVelocity = (int)((scaledAxis3 + scaledAxis4)*.75); //scaled (.75) for gear ratio.
-     int rightVelocity = (int)((scaledAxis3 - scaledAxis4)*.75); //scaled (.75) for gear ratio.
+//************************************************************
+     //calculate drivetrain velocity settings
+     int leftVelocity = (int)((scaledAxis3 + scaledAxis4)*.75); //scaled (.75) for gear ratio to match.
+     int rightVelocity = (int)((scaledAxis3 - scaledAxis4)*.75); //scaled (.75) for gear ratio to match.
      int rearWheelVelocity = scaledAxis3;
-            //arm motors
+     //arm motors velocity - move feed arms up/down
      int leftArmVelocity = (int)(scaledAxis2 * .50); //scaled (.5) *full power might be too much*
      int rightArmVelocity = (int)(scaledAxis2 * .50); //scaled (.5) *full power might be too much*
-
-
+     
+     //if R1 (bumper) is pressed, set velocity for 80%
+     //if R2 (bumper) is pressed, set velocity for -80%
+     int beltMotorVelocity = 0;
+     if(fakeXbox.ButtonR1.pressing()){
+       beltMotorVelocity = 80;
+     }else if(fakeXbox.ButtonR2.pressing()){
+       beltMotorVelocity = -80;
+     }
+     //if L1 (bumper) is pressed, set velocity for on
+     //if L2 (bumper) is pressed, set velocity for on
+     if(fakeXbox.ButtonL1.pressing()){
+       beltMotorVelocity = 0;
+     }else if(fakeXbox.ButtonL2.pressing()){
+       beltMotorVelocity = 0;
+     }
+//****************************************************************
      //set velocity
+    
      leftFWheelMotor.setVelocity(leftVelocity, vex::velocityUnits::pct);
      rightFWheelMotor.setVelocity(rightVelocity, vex::velocityUnits::pct);
      rearSlideWheels.setVelocity(rearWheelVelocity, vex::velocityUnits::pct);
-     //spin
+     //arms
+     leftArmMotor.setVelocity(leftArmVelocity, vex::velocityUnits::pct);
+     rightArmMotor.setVelocity(rightArmVelocity, vex::velocityUnits::pct);
+     //belt
+     leftBeltMotor.setVelocity(beltMotorVelocity, vex::velocityUnits::pct);
+     rightBeltMotor.setVelocity(beltMotorVelocity, vex::velocityUnits::pct);
+
+//*****************************************************************
+     //spin all
+     //wheels
      leftFWheelMotor.spin(vex::directionType::fwd);
      rightFWheelMotor.spin(vex::directionType::rev);
      rearSlideWheels.spin(vex::directionType::fwd);
+     //arms
+     leftArmMotor.spin(vex::directionType::fwd);
+     rightArmMotor.spin(vex::directionType::rev);
+     //belt (positive velocity aka R1 should feed in)(negative velocity pushes out.)
+     leftBeltMotor.spin(vex::directionType::fwd);
+     rightBeltMotor.spin(vex::directionType::rev);
+
+//*****************************************************
+  //Dolley Event
+  int dolleyState = 0;
+  if(fakeXbox.ButtonX.pressing()){
+    if(dolleyState == 0){ // moving from down to upright
+
+    }else{ //moving from upright to down
+    
+    }
+    
+  }
+
+
+
 
      //sleep
-     vex::task::sleep(20);
+     vex::task::sleep(10);
      
      if(fakeXbox.ButtonA.pressing()){
        return 1;
